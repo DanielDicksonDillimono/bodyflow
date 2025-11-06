a4736f69b83768a4cad3af1907ac11796afe0a3f
Daniel Dickson Dillimono <76996825+DanielDicksonDillimono@users.noreply.github.com>
2025-11-06 14:02:36 +0100

    Just and update

---
+++ lib/ui/main_pages/widgets/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Page'), centerTitle: false),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [Text('Welcome to BodyFlow Home Page!')],
+        ),
+      ),
+    );
+  }
+}
