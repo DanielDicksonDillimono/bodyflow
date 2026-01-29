a180d80a4d67579015e172026d794d6cc8d016a1
Daniel Dickson Dillimono <76996825+DanielDicksonDillimono@users.noreply.github.com>
2026-01-29 22:32:45 +0100

    update plist. with bundleId

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,6 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await FirebaseAppCheck.instance.activate(
-    providerWeb: ReCaptchaV3Provider('siteKey'),
     providerApple: AppleAppAttestProvider(),
     appleProvider: AppleProvider.appAttest,
   );
